f776a23f0464ad75123e84199ae684fa4bfa1e66
Subhoniddin <subhoniddin571@gmail.com>
2025-07-07 12:08:25 +0500

    web app

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,40 +3,10 @@ import Home from "../pages/home";
 import { ThemeProvider } from "./provider/theme-provider";
 import Product from "../pages/product";
 import CategoryPage from "@/pages/category";
-import TelegramWeb from "./telegramWeb";
-import { useEffect, useState } from "react";
 
 function App() {
-  const [isReady, setIsReady] = useState(false);
-  const [isTelegramUser, setIsTelegramUser] = useState(null);
-
-  useEffect(() => {
-    const init = async () => {
-      const tg = window.Telegram?.WebApp;
-      if (!tg) {
-        setIsTelegramUser(false);
-        setIsReady(true);
-        return;
-      }
-
-      tg.ready(); // WebApp tayyor
-      await new Promise((resolve) => setTimeout(resolve, 100)); // 100ms kutish
-
-      const user = tg.initDataUnsafe?.user;
-      setIsTelegramUser(!!user);
-      setIsReady(true);
-    };
-
-    init();
-  }, []);
-
-  if (!isReady || isTelegramUser === null) {
-    return <div>⏳ Yuklanmoqda...</div>;
-  }
-
-  if (!isTelegramUser) {
-    return <TelegramWeb />;
-  }
+  const telegramId = window.Telegram?.WebApp?.initDataUnsafe?.user?.id;
+  <p>{telegramId}</p>;
 
   return (
     <>
